4eef1f07ca04527f9bd335c82b9dfa00eeef5127
Chris Pham <chris.pham124@gmail.com>
2017-04-20 17:29:09 +0700

    First Commit

---
+++ views/components/display.vue
@@ -0,0 +1,24 @@
+<!-- TODO: Hãy viết thành một component hoàn chỉnh -->
+<template lang="html">
+        <div>
+          <img v-for="item in things" :src="'/public/image/' + item.photo" :alt="item.name" :style="img">
+        </div>
+</template>
+
+<script>
+export default {
+    props: ['things'],
+    data() {
+      return {
+        img : {
+          width : "128px",
+          height : "128px"
+       }
+      }
+    }
+}
+</script>
+
+<style lang="css">
+
+</style>
--- views/index_axios.vue
+++ views/index_axios.vue
@@ -1,21 +1,35 @@
 <template>
     <div id="app">
-        <select v-model="selected" name="selectOpt" v-on:change="getList()">
-            <option v-for="item in selectItems" :value="item.type">{{ item.name }}</option>
-        </select>
-        <div>
-            <p>Selected: {{selected}}</p>
-            <img v-for="item in things" :src="'/public/image/' + item.photo" :alt="item.name" width="128" height="128">
+        <div class="container">
+          <div class="row">
+            <h1 class="text text-center text-success">Vue Express</h1>
+            <hr/>
+            <select v-model="selected" name="selectOpt" v-on:change="getList()" class="form-control">
+                <option v-for="item in selectItems" :value="item.type">{{ item.name }}</option>
+            </select>
+            <div>
+                <h3 class="text-center text-danger">Selected: {{selected}}</h3>
+                <display :things="things" class="center-block col-md-12" ></display>
+            </div>
+          </div>
         </div>
     </div>
 </template>
 <script>
+
 	export default {
 		data() {
 			return {
 				selected: 0,
 				things: [],
-				errorMsg: ''
+				errorMsg: '',
+        thing : {
+          border : '1px solid red'
+        },
+        animal : {
+          border  :'1px solid green'
+        }
+
 			}
 		},
 
@@ -34,3 +48,5 @@
 		}
 	}
 </script>
+<style lang="css">
+</style>
